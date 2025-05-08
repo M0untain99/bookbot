16a46e0d195faf2b9aa6951fc4a89b54872d944c
M0untain99 <dmountain52@gmail.com>
2025-05-08 15:04:12 +0100

    User input allowed for text analysis

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from stats import count_words, count_chars, order_chars
+import sys
 
 def get_book_text(path):
     with open(path) as f:
@@ -8,14 +9,18 @@ def get_book_text(path):
 
 
 def __main__():
-    text = get_book_text("books/frankenstein.txt")
-    num_words = count_words(text)
-    chars = count_chars(text)
-    ordered = order_chars(chars)
+    if len(sys.argv) != 2:  # If there are not 2 command arguments
+        print(f'Usage: python3 main.py <path_to_book>')  # Explain how to use tool
+        sys.exit(1)  # Exit the program with error code 1
+
+    text = get_book_text(sys.argv[1])  # Send the book file entered by the user as a command argument
+    num_words = count_words(text)  # Count the number of words
+    chars = count_chars(text)  # Count how many time each character appears
+    ordered = order_chars(chars)  # Order the characters by frequency
 
     print(f'''
 ============ BOOKBOT ============
-Analyzing book found at books/frankenstein.txt...
+Analyzing book found at {sys.argv[1]}...
 ----------- Word Count ----------
 Found {num_words} total words
 --------- Character Count -------''')
